40e9d290d6db0904f0daf2d534eab53a4915056d
Matus Moravcik <Matus.Moravcik@gmail.com>
2012-05-22 01:09:11 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+"""
+Installation script:
+"""
+
+from setuptools import setup, find_packages
+
+from folioport import get_version
+
+
+setup(name='folioport',
+      version=get_version().replace(' ', '-'),
+      url='https://github.com/mmoravcik/folioport',
+      author="Matus Moravcik",
+      author_email="matus.moravcik@tangentlabs.co.uk",
+      description="Simple portfolio app for Django 1.3+",
+      long_description=open('README.rst').read(),
+      keywords="portfolio, gallery, showcase",
+      license='BSD',
+      platforms=['linux'],
+      packages=find_packages(exclude=["sandbox*", "tests*"]),
+      include_package_data=True,
+      install_requires=[
+          'django==1.4',
+          'PIL==1.1.7',
+          'South==0.7.3',
+          'sorl-thumbnail==11.12',
+          'django-mptt',
+          ],
+      # See http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=['Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: Unix',
+                   'Programming Language :: Python']
+      )
