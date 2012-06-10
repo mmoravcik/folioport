0d292bfb20daae00a4dcef103320c76a5169eb25
Matus Moravcik <Matus.Moravcik@gmail.com>
2012-06-10 23:04:02 +0100

    Summary and description is mandatory, some design changes

---
+++ folioport/project/migrations/0007_auto__chg_field_project_summary__chg_field_project_description.py
@@ -0,0 +1,74 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Changing field 'Project.summary'
+        db.alter_column('project_project', 'summary', self.gf('django.db.models.fields.TextField')(null=True))
+
+        # Changing field 'Project.description'
+        db.alter_column('project_project', 'description', self.gf('django.db.models.fields.TextField')(null=True))
+
+
+    def backwards(self, orm):
+        
+        # Changing field 'Project.summary'
+        db.alter_column('project_project', 'summary', self.gf('django.db.models.fields.TextField')(default=''))
+
+        # Changing field 'Project.description'
+        db.alter_column('project_project', 'description', self.gf('django.db.models.fields.TextField')(default=''))
+
+
+    models = {
+        'project.category': {
+            'Meta': {'object_name': 'Category'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'}),
+            'parent': ('mptt.fields.TreeForeignKey', [], {'blank': 'True', 'related_name': "'children'", 'null': 'True', 'to': "orm['project.Category']"}),
+            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '128', 'db_index': 'True'}),
+            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
+        },
+        'project.image': {
+            'Meta': {'object_name': 'Image'},
+            'caption': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date_modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'height': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'order': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
+            'project': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['project.Project']"}),
+            'width': ('django.db.models.fields.IntegerField', [], {'default': '300'})
+        },
+        'project.project': {
+            'Meta': {'object_name': 'Project'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'category': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['project.Category']", 'symmetrical': 'False'}),
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date_modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'order': ('django.db.models.fields.IntegerField', [], {'default': '-1'}),
+            'rating_score': ('django.db.models.fields.IntegerField', [], {'default': '0', 'blank': 'True'}),
+            'rating_votes': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0', 'blank': 'True'}),
+            'release_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '128', 'db_index': 'True'}),
+            'summary': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'tags': ('tagging.fields.TagField', [], {}),
+            'thumbnail': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'thumbnail_height': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'thumbnail_width': ('django.db.models.fields.IntegerField', [], {'default': '100'})
+        }
+    }
+
+    complete_apps = ['project']
