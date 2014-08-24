afc0c9a7f25a0570aa580fbc245c8c3ae6195216
Matus Moravcik <matus.moravcik@gmail.com>
2014-08-24 12:23:38 +0100

    Change fields for cms ItemFile

--- folioport/apps/cms/forms.py
+++ folioport/apps/cms/forms.py
@@ -1,4 +1,3 @@
-from ckeditor.widgets import CKEditorWidget
 from django import forms
 from django.db.models import get_model
 
@@ -7,12 +6,4 @@ class ItemImageForm(forms.ModelForm):
 
     class Meta:
         model = get_model('cms', 'ItemImage')
-
-
-class ItemRichText(forms.ModelForm):
-    text = forms.CharField(
-        widget=CKEditorWidget()
-    )
-    class Meta:
-        model = get_model('cms', 'ItemRichText')
-        fields = ('text',)
+        fields = ('image', 'caption', 'width', 'thumbnail_type')
