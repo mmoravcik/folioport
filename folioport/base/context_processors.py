7da45464395eb19565303962d00725358409c1c2
Matus Moravcik <matus.moravcik@gmail.com>
2014-09-29 20:47:28 +0100

    Initial attempt to include category in navigation

--- folioport/base/context_processors.py
+++ folioport/base/context_processors.py
@@ -8,9 +8,10 @@ Page = get_model('page', 'Page')
 
 def folioport_project(request):
     return {
-        'folioport_project_categories' : Category.objects.filter(active=1),
+        'folioport_project_categories' : Category.objects.active(),
         'GOOGLE_ANALYTICS_ACCOUNT': settings.GOOGLE_ANALYTICS_ACCOUNT,
         'folioport_projects': Project.objects.active(),
+        'folioport_projects_without_category': Project.objects.active().filter(category=None),
         'folioport_content_types': settings.FOLIOPORT_CONTENT_TYPES,
         'folioport_pages': Page.objects.active().exclude(type=Page.LANDING_PAGE)
     }
